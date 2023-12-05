877fc3f1940dab14521ae2df56a3d99f9c3997a7
Cheng-Yuan-Lai <a186235@g,ail.com>
2023-12-05 21:03:50 +0800

    fix: set SSL property for RDS connection

--- notificationService/utils/databasePool.js
+++ notificationService/utils/databasePool.js
@@ -13,6 +13,9 @@ const pool = new Pool({
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
+  ssl: {
+    rejectUnauthorized: false, // Important: only use this for self-signed certificates!
+  },
 });
 
 export default pool;
